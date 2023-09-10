1d913ee9a3d22b17585b80947a4cd02242c2994e
edan <ronndunster@gmail.com>
2023-09-10 13:29:13 -0700

    add files!

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {}
+
+export default function Footer({}: Props) {
+  return (
+    <div className="flex w-full flex-col items-center gap-4 border-t-[1px] p-5 text-sm text-gray-400 dark:border-gray-800 dark:text-gray-600">
+      Stop reading this!
+    </div>
+  )
+}
